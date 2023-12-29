b4422014c3ca973ac1d1bfbe60b29304e35e25f8
aymenBenAziza <aymenaziza542@gmail.com>
2023-12-29 13:40:07 +0100

    jdbc configuration

--- src/tn/tuniprob/gestionmagasin/DataBaseConnection.java
+++ src/tn/tuniprob/gestionmagasin/DataBaseConnection.java
@@ -1,4 +1,5 @@
 package tn.tuniprob.gestionmagasin;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,19 +8,21 @@ import java.sql.Statement;
 public class DataBaseConnection {
     public static void main(String[] args) {
         String dbURL = "jdbc:oracle:thin:@localhost:1522:xe";
-        String username = "system";
-        String password = "admin";
+        String username = "magasin_admin";  // Use the HR schema username
+        String password = "admin";  // Use the HR schema password
+
         try {
             Connection connection = DriverManager.getConnection(dbURL, username, password);
             System.out.println("Connected to the database:");
-            String sql =  "insert into employe " +
-                          "values(1, 'aymen ben aziza', 'beb ejdid', 500)";
+            String sql = "INSERT INTO employe VALUES (1, 'Aymen Ben Aziza', 'Beb Ejdid', 500)";
+
             Statement statement = connection.createStatement();
             int rows = statement.executeUpdate(sql);
+
             if (rows > 0)
-                System.out.println("a row has been inserted");
+                System.out.println("A row has been inserted into the data base");
             else
-                System.out.println("faild to insert a row");
+                System.out.println("Failed to insert a row");
 
         } catch (SQLException e) {
             System.out.println("Oops, connection error");
